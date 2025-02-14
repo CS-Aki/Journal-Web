7f6339222218a29573ae539838050a8d2e5a205e
CS-Aki <aquino.sir.victor.bscs2022@gmail.com>
2025-02-14 16:35:55 +0800

    Added CRUD Functionality For Notes, Will add frontend before proceeding deeper into backend

--- src/main/java/com/sirvic/journal/journal/controller/NotesController.java
+++ src/main/java/com/sirvic/journal/journal/controller/NotesController.java
@@ -3,10 +3,7 @@ package com.sirvic.journal.journal.controller;
 import com.sirvic.journal.journal.model.Notes;
 import com.sirvic.journal.journal.service.NotesService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,15 +18,36 @@ public class NotesController {
         this.notesService = notesService;
     }
 
-    @GetMapping
-    public List<Notes> displayAllNotes(){
-        return notesService.displayAllNotes();
+//    @GetMapping
+//    public List<Notes> displayAllNotes(){
+//        return notesService.displayAllNotes();
+//    }
+
+    @PostMapping(path = "{userId}")
+    public void createNote(@PathVariable("userId") Long userId,
+                           @RequestBody Notes note){
+         notesService.createNote(userId, note);
     }
 
-    @GetMapping(path = "{userId}")
-    public List<Notes> displayNotes(@PathVariable("userId") Long id){
-        return notesService.displayNotes(id);
+    @GetMapping(path = "user/{userId}")
+    public List<Notes> displayAllNotes(@PathVariable("userId") Long userId){
+        return notesService.displayAllNotes(userId);
     }
 
+    @GetMapping(path = "{id}")
+    public Notes displayNote(@PathVariable("id") Long noteId){
+        return notesService.displayNote(noteId);
+    }
 
+    @DeleteMapping(path = "{id}")
+    public void deleteNote(@PathVariable("id") Long noteId){
+        notesService.deleteNote(noteId);
+    }
+
+    @PatchMapping(path = "{id}")
+    public void editNote(@PathVariable("id") Long noteId,
+                         @RequestParam(required = false, defaultValue = "") String title,
+                         @RequestParam(required = false, defaultValue = "") String textBody){
+        notesService.editNote(noteId, title, textBody);
+    }
 }
--- src/main/java/com/sirvic/journal/journal/service/NotesService.java
+++ src/main/java/com/sirvic/journal/journal/service/NotesService.java
@@ -1,8 +1,8 @@
 package com.sirvic.journal.journal.service;
 
 import com.sirvic.journal.journal.model.Notes;
-import com.sirvic.journal.journal.model.Users;
 import com.sirvic.journal.journal.repository.NotesRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -19,9 +19,10 @@ public class NotesService {
         this.repository = repository;
     }
 
-    public List<Notes> displayNotes(Long id) {
 
-        List<Notes> notes = repository.findByUserId(id);
+    public List<Notes> displayAllNotes(Long userId) {
+
+        List<Notes> notes = repository.findByUserId(userId);
 
         if(notes.isEmpty()){
             throw new IllegalStateException("Note Not Found");
@@ -30,7 +31,47 @@ public class NotesService {
         return notes;
     }
 
-    public List<Notes> displayAllNotes() {
-        return repository.findAll();
+//    public List<Notes> displayAllNotes() {
+//        return repository.findAll();
+//    }
+
+    public void deleteNote(Long noteId) {
+        Optional<Notes> note = repository.findById(noteId);
+
+        if(note.isEmpty()){
+            throw new IllegalStateException("Note ID not found");
+        }
+
+        repository.delete(note.get());
+    }
+
+    @Transactional
+    public void editNote(Long noteId, String title, String textBody) {
+//        Optional<Notes> noteOpt = repository.findById(id);
+        Notes note = repository.findById(noteId).orElseThrow(() -> new IllegalStateException("Note ID not Found"));
+        String origTitle = note.getTitle();
+        String origBody = note.getTextBody();
+
+        if(title == "" || title.equals(origTitle)){
+            throw new IllegalStateException("No Changes Made");
+        }
+
+        if(textBody == null || textBody.equals(origBody)){
+            throw new IllegalStateException("No Changes Made");
+        }
+
+        note.setTitle(origTitle.equals(title) ? origTitle : title);
+        note.setTextBody(origBody.equals(textBody) ? origBody : textBody);
+    }
+
+    @Transactional
+    public void createNote(Long userId, Notes note) {
+        note.setUserId(userId);
+        repository.save(note);
+    }
+
+    public Notes displayNote(Long noteId) {
+        Notes note = repository.findById(noteId).orElseThrow(() -> new IllegalStateException("Note ID not found"));
+        return note;
     }
 }
