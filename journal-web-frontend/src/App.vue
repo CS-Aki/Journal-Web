917925ed042061eae8247f1138e09490b99a2bdc
CS-Aki <aquino.sir.victor.bscs2022@gmail.com>
2025-02-17 21:16:35 +0800

    WIP Landing Page, still practicing fundamentals for css, will use tailwind maybe on a future project or this project

--- journal-web-frontend/src/App.vue
+++ journal-web-frontend/src/App.vue
@@ -3,5 +3,19 @@
 </script>
 
 <template>
-  <h1>Journal Web</h1>
+  <header id="index-header">
+    <h1>The Journey</h1>
+    <button id="signup-btn">Sign Up</button>
+    <button id="login-btn">Log In</button>
+  </header>
+
+  <div id="box1">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe obcaecati fugiat quidem alias hic officia eius, quia exercitationem consectetur tempora dolorum quasi minus eligendi consequatur aliquid nobis! Facilis, nihil dolorum?
+  </div>
+
+  <div id="box2">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis perspiciatis tempore culpa quisquam amet qui, aliquid voluptates non doloribus ad numquam est nisi obcaecati fugiat suscipit exercitationem nesciunt, quas eligendi!
+  </div>
+
+
 </template>
