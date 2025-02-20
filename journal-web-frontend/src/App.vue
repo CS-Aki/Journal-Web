58544d396b2d100d292a3b0d56edba9e4d3fdbb6
CS-Aki <aquino.sir.victor.bscs2022@gmail.com>
2025-02-20 09:59:18 +0800

    Imported Bootstrap and will now add modal for registration and login

--- journal-web-frontend/src/App.vue
+++ journal-web-frontend/src/App.vue
@@ -1,5 +1,16 @@
-<script>
-
+ <script>
+  export default{
+    methods: {
+      displayLoginModal(){
+        this.$refs["login-modal"].show();
+        console.log("Display Login");
+      },
+      hideLoginModal(){
+        this.$refs["login-modal"].hide();
+        console.log("Hide Login");
+      }
+    }
+  }
 </script>
 
 <template>
@@ -9,17 +20,27 @@
         <div id="header-logo">
           <!-- Will change this to image later on -->
           <h1>The Journey</h1> 
-        </div>
+        </div> 
        </a>
       <div class="nav-header-btn">
         <button id="contacts-btn">Contacts</button>
         <button id="about-btn">About Us</button>
-        <button id="signup-btn" type="button">Sign Up</button>
-        <button id="login-btn" type="button">Log In</button>
+        <button class="" id="signup-btn" type="button">Sign Up</button>
+        <button class="" id="login-btn" type="button">Log In</button>
       </div>
     </header>
 
+    <div>
+      <b-button id="show-btn" @click="displayLoginModal">Open Modal</b-button>
+      <b-button id="toggle-btn" @click="toggleModal">Toggle Modal</b-button>
 
-  
+      <b-modal ref="login-modal" hide-footer title="Using Component Methods">
+        <div class="d-block text-center">
+          <h3>Hello From My Modal!</h3>
+        </div>
+        <b-button class="mt-3" variant="outline-danger" block @click="hideLoginModal">Close Me</b-button>
+      </b-modal>
+    </div>
+    
 
 </template>
